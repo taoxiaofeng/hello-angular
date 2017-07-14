ea137e499a86d6cc4aa3124ecfbda05cddb1c84e
taoxf <taoxiaofeng55@qq.com>
2017-07-14 23:56:15 +0800

    初次认识angular2

---
+++ app/environment_app.component.ts
@@ -0,0 +1,16 @@
+import { Component, View } from '@angular2/core';
+
+@Component({
+	selector:'my-app'
+})
+
+@View({
+	template:'<h1>My First Angular2 App</h2>'
+})
+
+export class AppComponent{
+	
+	constructor(argument) {
+		// code...
+	}
+}
