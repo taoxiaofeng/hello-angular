fbed1fc3f622ed0ee5665bc89d2f89af0f8e632f
taoxf <taoxiaofeng55@qq.com>
2017-07-21 19:51:24 +0800

    英雄编辑器

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+	id:number;
+	name:string;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  	title = 'Tour of Heroes';
+  	// hero = 'windstorm';
+
+  	hero:Hero = {
+	id:1,
+	name:'hello'	
+	}
 }
