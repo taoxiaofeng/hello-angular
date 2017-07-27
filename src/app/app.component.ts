6524b5c8a764d63c35711ff37a82216c7a9799f0
taoxf <taoxiaofeng55@qq.com>
2017-07-27 20:53:42 +0800

    主从结构

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ export class Hero {
 	name:string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 
 @Component({
   selector: 'app-root',
@@ -14,9 +27,10 @@ export class Hero {
 export class AppComponent {
   	title = 'Tour of Heroes';
   	// hero = 'windstorm';
+    heroes = HEROES;
+    selectedHero:Hero;
 
-  	hero:Hero = {
-	id:1,
-	name:'hello'	
-	}
+    onSelect(hero: Hero):void {
+       this.selectedHero = hero;
+    }
 }
