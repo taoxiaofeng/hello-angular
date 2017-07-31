0695646cdfde7dadc249111323edc8336a9008ba
taoxf <taoxiaofeng55@qq.com>
2017-07-31 20:24:09 +0800

    多个组件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
-export class Hero {
-	id:number;
-	name:string;
-}
+// export class Hero {
+// 	id:number;
+// 	name:string;
+// }
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
