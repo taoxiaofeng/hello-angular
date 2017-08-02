2faceab00d70f7f9e5a4d6206412ab499dd4574d
taoxf <taoxiaofeng55@qq.com>
2017-08-02 20:55:59 +0800

    服务

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-
+import { HeroService } from './hero.service';
 // export class Hero {
 // 	id:number;
 // 	name:string;
 // }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+// const HEROES: Hero[] = [
+//   { id: 11, name: 'Mr. Nice' },
+//   { id: 12, name: 'Narco' },
+//   { id: 13, name: 'Bombasto' },
+//   { id: 14, name: 'Celeritas' },
+//   { id: 15, name: 'Magneta' },
+//   { id: 16, name: 'RubberMan' },
+//   { id: 17, name: 'Dynama' },
+//   { id: 18, name: 'Dr IQ' },
+//   { id: 19, name: 'Magma' },
+//   { id: 20, name: 'Tornado' }
+// ];
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   	title = 'Tour of Heroes';
   	// hero = 'windstorm';
-    heroes = HEROES;
+    // heroes = HEROES;
+    heroes : Hero[];
     selectedHero:Hero;
+
+    constructor(private heroService:HeroService){}
+    ngOnInit(){
+      this.getHeroes();
+    }
+
+    getHeroes():void {
+      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 
     onSelect(hero: Hero):void {
        this.selectedHero = hero;
