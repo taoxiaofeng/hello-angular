2e5c24f97202b999ab050e25a62d8a6043b20283
taoxiaofeng <taoxf@ipharmacare.net>
2017-08-30 21:16:09 +0800

    路由

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -7,20 +7,19 @@ import { HeroService } from './hero.service';
 
 @Component({
 	selector: 'hero-detail',
-	template: `
-	<div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>`
+	templateUrl:'./hero-detail.component.html';
+	styleUrls:['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-	constructor() {}
+	constructor(
+		private heroService:HeroService,
+		private route:ActivatedRoute,
+		private location:Location
+	) {}
 	@Input() hero:Hero;
-	ngOnInit() {
-		
+	ngOnInit():void {
+		this.route.paramMap
+		.switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+		.subscribe();
 	}
 }
