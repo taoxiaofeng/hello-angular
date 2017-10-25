246dc0f7706b41bc4be60196a392d6ab78d14004
taoxiaofeng <taoxf@ipharmacare.net>
2017-10-25 14:37:03 +0800

    angular 服务

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,13 @@ import { Component } from '@angular/core';
   template: `
     <h1>{{title}}</h1>
     <nav>
-    	<a routerLink="/deshboard" routerLinkActive="active">Dashboard</a>
-    	<a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
     <router-outlet></router-outlet>
   `,
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
 }
-
