2ec6f209b68e0fc8944f0b28be36b09c633f58ae
taoxiaofeng <taoxf@ipharmacare.net>
2018-02-06 16:26:04 +0800

    样式调整

--- src/app/common/my-toolbar/my-toolbar.component.ts
+++ src/app/common/my-toolbar/my-toolbar.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./my-toolbar.component.css']
 })
 export class MyToolbarComponent implements OnInit {
+    private host = window.location.href;
+    private angularLogoImg='../../../assets/images/logo-nav@2x.png';
     constructor() { }
 
     ngOnInit() { }
--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
     selector: 'my-home',
     template:`
     <h1 class="welcome">欢迎使用</h1>
-    <div class="welcome-img"><img src="{{welcome}}"></div>
+    <div class="welcome-img">
+        
+    </div>
     `,
     styles:[`
     .welcome,
@@ -14,7 +16,7 @@ import { Component, OnInit } from '@angular/core';
 }) 
 export class HomeComponent implements OnInit {
     private host = location.host;
-    private welcome = '../images/welcome.jpg';
+    private welcome = '../assets/images/welcome.jpg';
     constructor() { }
 
     ngOnInit() { }
