906c7ccee92951758cfd0b99dc083a44f0878ac0
taoxf <taoxf@ipharmacare.net>
2019-02-21 10:51:10 +0800

    菜单选中

--- src/app/common/mat-sidenav/mat-sidenav.component.ts
+++ src/app/common/mat-sidenav/mat-sidenav.component.ts
@@ -3,66 +3,66 @@ import { Router } from "@angular/router";
 import { Menus } from "../../menu/menu";
 
 @Component({
-  selector: "mat-sidenav",
-  templateUrl: "./mat-sidenav.component.html",
-  styleUrls: ["./mat-sidenav.component.css"]
+    selector: "mat-sidenav",
+    templateUrl: "./mat-sidenav.component.html",
+    styleUrls: ["./mat-sidenav.component.css"]
 })
 export class MatSidenavComponent implements OnInit {
-  @Input() matSidenavStatus: boolean = true;
-  @Input() menus: Menus;
-  @Input() selectMenu: any;
-  // public menuUnfold:boolean = false;
-  public selectSubMenu: any;
-  public selectThreeMenu: any;
-  constructor(public router: Router) {}
+    @Input() matSidenavStatus: boolean = true;
+    @Input() menus: Menus;
+    @Input() selectMenu: any;
+    // public menuUnfold:boolean = false;
+    public selectSubMenu: any;
+    public selectThreeMenu: any;
+    constructor(public router: Router) { }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
-    //Add '${implements OnChanges}' to the class.
-    console.log("chanages", changes);
-    if (changes.selectMenu && changes.selectMenu.currentValue) {
-      let curValue = changes.selectMenu.currentValue.children;
-      if (curValue.children && curValue.children.length > 0) {
-        // this.selectSubMenu = changes.selectMenu.currentValue.children[0];
-        this.checkedMenus(
-          "subMenu",
-          changes.selectMenu.currentValue.children[0]
-        );
-        if (changes.selectMenu.currentValue.children[0].children) {
-          // this.selectThreeMenu = changes.selectMenu.currentValue.children[0].children[0];
-          this.checkedMenus(
-            "threeMenu",
-            changes.selectMenu.currentValue.children[0].children[0]
-          );
+    ngOnChanges(changes: SimpleChanges): void {
+        //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+        //Add '${implements OnChanges}' to the class.
+        console.log("chanages", changes);
+        if (changes.selectMenu && changes.selectMenu.currentValue) {
+            let curValue = changes.selectMenu.currentValue.children;
+            if (curValue.children && curValue.children.length > 0) {
+                // this.selectSubMenu = changes.selectMenu.currentValue.children[0];
+                this.checkedMenus(
+                    "subMenu",
+                    changes.selectMenu.currentValue.children[0]
+                );
+                if (changes.selectMenu.currentValue.children[0].children) {
+                    // this.selectThreeMenu = changes.selectMenu.currentValue.children[0].children[0];
+                    this.checkedMenus(
+                        "threeMenu",
+                        changes.selectMenu.currentValue.children[0].children[0]
+                    );
+                }
+            }
         }
-      }
     }
-  }
-  ngOnInit() {}
+    ngOnInit() { }
 
-  //展开菜单
-  checkedMenus(hierarchy: string, menu: any) {
-    if (!menu) {
-      return;
-    }
-    console.log(menu);
-    switch (hierarchy) {
-      case "subMenu":
-        if (
-          !this.selectSubMenu ||
-          (this.selectSubMenu && this.selectSubMenu.name != menu.name)
-        ) {
-          this.selectSubMenu = menu;
-        } else {
-          this.selectSubMenu = {};
+    //展开菜单
+    checkedMenus(hierarchy: string, menu: any) {
+        if (!menu) {
+            return;
         }
-        break;
-      case "threeMenu":
-        this.selectThreeMenu = menu;
-        this.router.navigate([menu.url]);
-        break;
-      default:
-        break;
+        console.log(menu);
+        switch (hierarchy) {
+            case "subMenu":
+                if (
+                    !this.selectSubMenu ||
+                    (this.selectSubMenu && this.selectSubMenu.name != menu.name)
+                ) {
+                    this.selectSubMenu = menu;
+                } else {
+                    this.selectSubMenu = {};
+                }
+                break;
+            case "threeMenu":
+                this.selectThreeMenu = menu;
+                this.router.navigate([menu.url]);
+                break;
+            default:
+                break;
+        }
     }
-  }
 }
