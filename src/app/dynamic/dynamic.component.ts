663b852fe8580799347ddb4eb5a2f6ad35b7cadd
taoxf <taoxf@ipharmacare.net>
2019-03-05 15:48:48 +0800

    修改端口

--- src/app/dynamic/dynamic.component.ts
+++ src/app/dynamic/dynamic.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
 
 @Component({
-    selector: 'dynamic',
-    templateUrl: './dynamic.component.html',
-    styleUrls: ['./dynamic.component.css']
+  selector: "dynamic",
+  templateUrl: "./dynamic.component.html",
+  styleUrls: ["./dynamic.component.css"]
 })
 export class DynamicComponent implements OnInit {
-    directiveShow: boolean = false;
-    templateShow: boolean = false;
-    color: string = '';
-    constructor() { }
+  title: string = "";
+  directiveShow: boolean = false;
+  templateShow: boolean = false;
+  color: string = "";
+  constructor() {}
 
-    currentHero: Hero;
-    heroImageUrl = './../../assets/images/';
+  currentHero: Hero;
+  heroImageUrl = "./../../assets/images/";
 
-    ngOnInit() { }
-
+  ngOnInit() {}
 }
-
-
